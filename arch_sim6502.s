09d9126661170d0a96e1bc1c1eb17b0db115071a
Willis Blackburn <wboyce@panix.com>
2022-02-25 10:31:19 -0500

    Add back debug handler

--- arch_sim6502.s
+++ arch_sim6502.s
@@ -104,3 +104,93 @@ putchar:
         ldx     #>io_char
         ldy     #1
         jmp     write
+
+; Debugging helpers
+
+.zeropage
+
+save_pc: .res 2
+
+.bss
+
+save_a: .res 1
+save_x: .res 1
+save_y: .res 1
+save_sp: .res 1
+
+.macro  push8   value
+        lda     value
+        pha 
+.endmacro
+
+.macro  push16  value
+        push8   value
+        push8   value+1
+.endmacro
+
+.macro  pull8   value
+        pla
+        sta     value
+.endmacro
+
+.macro  pull16  value
+        pull8   value+1
+        pull8   value
+.endmacro
+
+.code
+
+format: .byte "$%02X: A=%02X X=%02X Y=%02X SP=%02X", $0A, $00
+
+; Prints the register values to stderr.
+debug_handler:
+        cld                     ; Clear decimal flag (just in case)
+        sta     save_a          ; Save the registers
+        stx     save_x
+        sty     save_y
+        tsx                     ; Get stack pointer into X
+        stx     save_sp         ; Save it so we can print it
+        ldy     $102,x          ; PC low byte
+        sty     save_pc
+        ldy     $103,x          ; PC high byte
+        dey                     ; Subtract 256 from PC; we will index with Y = 255 to get PC-1
+        sty     save_pc+1
+        push8   tmp1
+        push8   tmp2
+        push8   tmp3
+        push8   tmp4
+        push16  ptr1  
+        push16  ptr2
+        push16  sreg
+        push16  regsave  
+        lda     _stderr         ; fprintf(stderr, ...
+        ldx     _stderr+1
+        jsr     pushax
+        lda     #<format        ; format, ...
+        ldx     #>format
+        jsr     pushax
+        ldy     #$FF
+        lda     (save_pc),y     ; id, ...
+        jsr     pusha0          
+        lda     save_a          ; A, ...
+        jsr     pusha0
+        lda     save_x          ; X, ...
+        jsr     pusha0
+        lda     save_y          ; Y, ...
+        jsr     pusha0
+        lda     save_sp         ; SP)
+        jsr     pusha0
+        ldy     #14             ; 14 bytes on the C stack
+        jsr     _fprintf
+        pull16  regsave
+        pull16  sreg
+        pull16  ptr2
+        pull16  ptr1
+        pull8   tmp4
+        pull8   tmp3
+        pull8   tmp2
+        pull8   tmp1
+        lda     save_a
+        ldx     save_x
+        ldy     save_y
+        rti
