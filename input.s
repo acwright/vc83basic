0b354ab3f47f4df4568226936ead7e6196cc79a3
Willis Blackburn <wboyce@panix.com>
2022-08-07 17:50:13 -0400

    Make input a couple of instructions shorter

--- input.s
+++ input.s
@@ -5,8 +5,6 @@
 
 .assert TOKEN_END_REPEAT = 0, error
 
-retry_with_prompt:
-        dec     lp                      ; After failing to read separator, back up to retry after new prompt
 exec_input:
         lda     #'?'                    ; Prepare to print '?' prompt
         jsr     putchar
@@ -15,18 +13,16 @@ exec_input:
 @next_var:
         jsr     decode_byte             ; Read the variable
         beq     @done                   ; It was TOKEN_END_REPEAT, nothing more to read
-        tay                             ; Variable is safe in Y
-        lda     bp                      ; Check the read position
-        beq     @skip_separator         ; If read position is 0 then no separator needed
-        jsr     parse_argument_separator    ; We read something from ths line so need a ',' to continue
-        bcs     retry_with_prompt       ; Didn't find ',' so issue a new prompt
-@skip_separator:
-        tya                             ; Variable value back into A
         jsr     set_variable_value_ptr  ; Sets variable_value_ptr to the storage for this variable
         jsr     read_number             ; Returns value in AX
         bcs     @error                  ; Failed to read a number
         jsr     set_variable_value      ; Store the value
-        jmp     @next_var
+        jsr     decode_byte             ; Read the variable
+        beq     @done                   ; It was TOKEN_END_REPEAT, nothing more to read
+        dec     lp                      ; Otherwise back up
+        jsr     parse_argument_separator    ; We read something from ths line so need a ',' to continue
+        bcs     exec_input              ; Didn't find ',' so issue a new prompt
+        jmp     @next_var               ; Otherwise just read the next variable
 
 @done:
         clc
