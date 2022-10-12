75abad81b67727f06c32473c35422ede6e5c0bda
Willis Blackburn <wboyce@panix.com>
2022-10-12 17:29:48 -0400

    Move putchar_buffer to list.s

--- list.s
+++ list.s
@@ -225,10 +225,23 @@ add_whitespace:
         beq     @done                   ; Just return if it's zero
         lda     buffer-1,x              ; Get buffer[x-1]
         cmp     #')'                    ; Is it ')'?
-        beq     @add                    ; Yes, add a space
+        beq     putchar_space_buffer    ; Yes, add a space
         jsr     is_name_character       ; Is it a name character?
-        bcc     @add                    ; Yes
+        bcc     putchar_space_buffer    ; Yes
 @done:
         rts
-@add:
-        jmp     putchar_space_buffer
+
+; Writes a single byte to buffer at position bp and increments bp.
+; Does not check for buffer overflow; we assume this can't happen.
+; STA is the last operation so zero flag will be set if we wrote zero.
+; A = the byte to write (preserved)
+; bp = the buffer position (updated)
+; Y SAFE, BC SAFE, DE SAFE
+
+putchar_space_buffer:
+        lda     #' '
+putchar_buffer:
+        ldx     bp                      ; Load position
+        inc     bp                      ; Incrment position
+        sta     buffer,x                ; Store A in buffer
+        rts
