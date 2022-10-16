05ace7538ae82a6547079f9cd4ec3af9c0604735
Willis Blackburn <wboyce@panix.com>
2022-10-16 11:50:33 -0400

    More and better tests

--- expression_test.c
+++ expression_test.c
@@ -8,12 +8,38 @@ static void set_line(const char* data, size_t length) {
     lp = (char)offsetof(Line, data);
 }
 
-static void test_one_op(char op, int expected11, int expected12, int expected21) {
+static void test_one_op(char op, int expected00, int expected01, int expected10, int expected11) {
     char err;
     int result;
     char line_data[] = { TOKEN_NUM, 0x00, 0x00, 0, TOKEN_NUM, 0x00, 0x00, TOKEN_NO_VALUE };
 
+    DEBUG(op);
+
     line_data[3] = TOKEN_OP | op;
+    line_data[1] = 0;
+    line_data[5] = 0;
+    set_line(line_data, sizeof line_data);
+    err = evaluate_expression();
+    ASSERT_EQ(err, 0);
+    result = pop_value();
+    ASSERT_EQ(result, expected00);
+
+    line_data[1] = 0;
+    line_data[5] = 1;
+    set_line(line_data, sizeof line_data);
+    err = evaluate_expression();
+    ASSERT_EQ(err, 0);
+    result = pop_value();
+    ASSERT_EQ(result, expected01);
+
+    line_data[1] = 1;
+    line_data[5] = 0;
+    set_line(line_data, sizeof line_data);
+    err = evaluate_expression();
+    ASSERT_EQ(err, 0);
+    result = pop_value();
+    ASSERT_EQ(result, expected10);
+
     line_data[1] = 1;
     line_data[5] = 1;
     set_line(line_data, sizeof line_data);
@@ -21,28 +47,14 @@ static void test_one_op(char op, int expected11, int expected12, int expected21)
     ASSERT_EQ(err, 0);
     result = pop_value();
     ASSERT_EQ(result, expected11);
-
-    line_data[1] = 1;
-    line_data[5] = 2;
-    set_line(line_data, sizeof line_data);
-    err = evaluate_expression();
-    ASSERT_EQ(err, 0);
-    result = pop_value();
-    ASSERT_EQ(result, expected12);
-
-    line_data[1] = 2;
-    line_data[5] = 1;
-    set_line(line_data, sizeof line_data);
-    err = evaluate_expression();
-    ASSERT_EQ(err, 0);
-    result = pop_value();
-    ASSERT_EQ(result, expected21);
 }
 
 static void test_one_unary_op(char op, int expected0, int expected1) {
     char err;
     int result;
     char line_data[] = { 0, TOKEN_NUM, 0x00, 0x00, TOKEN_NO_VALUE };
+
+    DEBUG(op);
 
     line_data[0] = TOKEN_UNARY_OP | op;
     line_data[2] = 0;
@@ -62,15 +74,18 @@ static void test_one_unary_op(char op, int expected0, int expected1) {
 
 static void test_evaluate_expression(void) {
 
-    test_one_op(OP_ADD, 2, 3, 3);
-    test_one_op(OP_SUB, 0, -1, 1);
+    test_one_op(OP_ADD, 0, 1, 1, 2);
+    test_one_op(OP_SUB, 0, -1, 1, 0);
 
-    test_one_op(OP_EQ, 1, 0, 0);
-    test_one_op(OP_NE, 0, 1, 1);
-    test_one_op(OP_LT, 0, 1, 0);
-    test_one_op(OP_LE, 1, 1, 0);
-    test_one_op(OP_GT, 0, 0, 1);
-    test_one_op(OP_GE, 1, 0, 1);
+    test_one_op(OP_EQ, 1, 0, 0, 1);
+    test_one_op(OP_NE, 0, 1, 1, 0);
+    test_one_op(OP_LT, 0, 1, 0, 0);
+    test_one_op(OP_LE, 1, 1, 0, 1);
+    test_one_op(OP_GT, 0, 0, 1, 0);
+    test_one_op(OP_GE, 1, 0, 1, 1);
+
+    test_one_op(OP_AND, 0, 0, 0, 1);
+    test_one_op(OP_OR, 0, 1, 1, 1);
 
     test_one_unary_op(UNARY_OP_MINUS, 0, -1);
     test_one_unary_op(UNARY_OP_NOT, 1, 0);
