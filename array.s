5144967bb4f6b05ed540c631f4c5c82196eac78f
Willis Blackburn <wboyce@panix.com>
2025-01-04 18:49:38 -0500

    Small renames

--- array.s
+++ array.s
@@ -19,12 +19,12 @@ exec_dim:
         ldx     #0                      ; AX is the 16-bit size
         jsr     int_to_fp               ; Load as float into FP0
         jsr     push_fp0                ; Push onto the stack
-        bcs     @error                  ; Out of space
+        bcs     @done                   ; Out of space
 @next:
         jsr     evaluate_expression     ; Evaluate the next expression; the value is now on the stack
-        bcs     @error
+        bcs     @done
         jsr     push_value_1            ; We have to add one to the value
-        bcs     @error
+        bcs     @done
         jsr     op_add
         jsr     op_mul                  ; Multiply the two stack values together
         ldy     line_pos
@@ -32,9 +32,8 @@ exec_dim:
         bne     @next                   ; Keep decoding more dimensions
         jsr     truncate_fp_to_int
         debug $00
-        bcs     @error                  ; Value was too large
-        bmi     @error                  ; Value was negative
-        jsr
+        bcs     @done                   ; Value was too large
+        bmi     @done                   ; Value was negative
         
 
         
