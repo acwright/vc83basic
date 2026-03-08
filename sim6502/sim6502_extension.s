bfb0e7b8fb70d9598455ced80513f16511db59f7
Willis Blackburn <wboyce@panix.com>
2026-03-07 19:26:03 -0500

    VER$ function for sim6502

--- sim6502/sim6502_extension.s
+++ sim6502/sim6502_extension.s
@@ -19,7 +19,7 @@ exec_bye:
         jmp     exit
 
 ex_function_name_table:
-        name_table_entry "VER"
+        name_table_entry "VER$"
 :       name_table_end
 
 ex_function_arity_table:
@@ -28,8 +28,21 @@ ex_function_arity_table:
 ex_function_vectors:
         .word   fun_ver-1
 
+version:
+.include "version.inc"
+version_length = * - version
+
 fun_ver:
         jsr     pop_fp0                 ; Ignore argument
-        lday    #fp_one
-        jsr     load_fp0
-        jmp     push_fp0
+        lda     #version_length
+        jsr     string_alloc
+        sta     dst_ptr                 ; New string is destination for the copy
+        inc     dst_ptr                 ; Move past length byte
+        bne     @skip_iny
+        iny
+@skip_iny:
+        sty     dst_ptr+1
+        ldax    #version
+        ldy     #version_length
+        jsr     copy_y_from
+        jmp     push_string
